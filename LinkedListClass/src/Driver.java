a427dbe2357f18bbcb0e612fbdec6135e3752ace
ASUS <ASUS@Laiths-ASUS>
2025-03-15 01:50:37 +0200

    updated

--- LinkedListClass/src/Driver.java
+++
@@ -1,9 +0,0 @@
-
-public class Driver {
-	public static void main(String[] args) {
-		LinkedList list = new LinkedList();
-		list.insert("Laith");
-		list.insert("Ali");
-		list.display();
-	}
-}
--- LinkedListClass/src/LinkedList.java
+++ LinkedListClass/src/LinkedList.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class LinkedList<T> {
 	Node head;
@@ -15,7 +17,7 @@ public class LinkedList<T> {
 		current.next = node;
 	}
 
-	public void delete(T data) {
+	public void deleteIteratively(T data) {
 		if (head == null)
 			return;
 
@@ -34,14 +36,83 @@ public class LinkedList<T> {
 		}
 	}
 
-	public void display() {
+	public void deleteRecursively(T data) {
+		head = deleteRecursivelyHelper(head, data);
+	}
+
+	private Node deleteRecursivelyHelper(Node current, T data) {
+		if (current == null) {
+			return null;
+		}
+
+		if (current.data.equals(data)) {
+			return current.next;
+		}
+
+		current.next = deleteRecursivelyHelper(current.next, data);
+		return current;
+	}
+
+	public void traverseForward() {
 		Node current = head;
 		int i = 1;
 		while (current != null) {
-			System.out.println(i + " " + current.data);
+			System.out.println(i + ". " + current.data);
 			current = current.next;
 			i++;
 		}
+	}
+
+	public void traverseBackwards() {
+		ArrayList<T> list = new ArrayList<>();
+		Node current = head;
+		while (current != null) {
+			list.add((T) current.data);
+			current = current.next;
+		}
+		Collections.reverse(list);
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println((i + 1) + ". " + list.get(i));
+		}
+	}
+
+	public void search(T data) {
+		if (head == null) {
+			return;
+		}
+
+		Node current = head;
+		while (current != null) {
+			if (current.data.equals(data)) {
+				System.out.println("Entity Found: " + current.data);
+				return;
+			}
+
+			current = current.next;
+		}
+		System.out.println("Entity not found");
+	}
+
+	public void deleteAll() {
+		Node current = head;
+		while (current != null) {
+			if (current.next != null)
+				current.next = current.next.next;
+		}
+	}
+
+	public int size() {
+		if (head == null || head.next == null)
+			return 0;
+
+		int i = 0;
+		Node current = head;
+		while (current != null) {
+			i++;
+			current = current.next;
+		}
+
+		return i;
 	}
 
 }
